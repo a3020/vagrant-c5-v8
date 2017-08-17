fd43545db0090578d6979fec540b908169d58430
a3020 <a3020@fastmail.com>
2017-08-17 14:24:13 +0200

    Replace max_filesize and max_exec time

--- post.sh
+++ post.sh
@@ -3,6 +3,8 @@
 cd /vagrant
 mkdir public
 
+apt-get update
+
 echo "## Install utilities"
 apt-get -y install unzip
 
@@ -23,5 +25,13 @@ chmod +x concrete/bin/concrete5
 concrete/bin/concrete5 c5:install --db-server=localhost --db-username=c5 --db-password=c5 --db-database=c5 \
 	--admin-email=admin@example.com --admin-password=admin \
 	--starting-point=elemental_blank
+
+sed -i 's/upload_max_filesize\s*=.*/upload_max_filesize=20M/g' /etc/php/7.1/fpm/php.ini
+sed -i 's/max_execution_time\s*=.*/max_execution_time=100/g' /etc/php/7.1/fpm/php.ini
+service php7.1-fpm restart
+
+# vi /etc/nginx/nginx.conf
+# client_max_body_size 100m;
+systemctl restart nginx
 
 echo "## Done"
